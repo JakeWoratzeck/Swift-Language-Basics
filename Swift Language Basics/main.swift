9c47921c723ff41770d43f1133c8cd39452eca25
Jake Woratzeck <jakeworatzeck@Jakes-MacBook-Pro-4.local>
2017-09-02 15:26:25 -0500

    First commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Jake Woratzeck on 9/2/17.
+//  Copyright © 2017 Jake Woratzeck. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
